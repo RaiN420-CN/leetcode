0bd0709a6a7f2a0ea6ee9223a1b68ce3e180babf
zhanghaoyu45 <zhanghaoyua45@jd.com>
2022-02-17 15:52:18 +0800

    day1

---
+++ solutions/twoSumSolution.java
@@ -0,0 +1,25 @@
+package solutions;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class twoSumSolution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 14};
+        int target = 9;
+        twoSumSolution solution = new twoSumSolution();
+        System.out.println(Arrays.toString(solution.twoSum(nums,target)));
+    }
+}
+
+
