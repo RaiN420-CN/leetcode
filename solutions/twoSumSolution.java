aecf84179618bc4a927aa13230720ca99ce7c81c
zhy <zhanghaoyua@126.com>
2022-02-27 18:38:20 +0800

    2.27

--- solutions/twoSumSolution.java
+++ solutions/twoSumSolution.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class twoSumSolution {
+    //两数之和
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++) {
@@ -14,6 +15,7 @@ public class twoSumSolution {
         }
         return new int[0];
     }
+    //test
     public static void main(String[] args) {
         int[] nums = new int[]{2, 7, 11, 14};
         int target = 9;
