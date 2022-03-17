345b6342a9628e42b7877b26a70f8793459a29a6
zhanghaoyu45 <zhanghaoyu45@jd.com>
2022-03-17 18:59:54 +0800

    3.17

--- solutions/longestWordsSolution.java
+++ solutions/longestWordsSolution.java
@@ -1,11 +1,23 @@
 package solutions;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 //720. 词典中最长的单词
 public class longestWordsSolution {
     public String longestWord(String[] words) {
-        String test = "123";
+        Map<String,Integer> map = new HashMap<>();
+        int max = 0;
+        for (String word : words) {
+            map.put(word, word.length());
+            if(word.length() > max){
+                max = word.length();
+            }
+        }
+        for(;max > 1 ; max--){
+
+        }
 
         return "";
     }
