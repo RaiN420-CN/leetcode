f6829180388dbf138306305b5b2045e572537d6b
zhanghaoyu45 <zhanghaoyu45@jd.com>
2022-03-17 18:24:37 +0800

    3.17

---
+++ solutions/longestWordsSolution.java
@@ -0,0 +1,16 @@
+package solutions;
+
+import java.util.Arrays;
+
+//720. 词典中最长的单词
+public class longestWordsSolution {
+    public String longestWord(String[] words) {
+        String test = "123";
+
+        return "";
+    }
+    public static void main(String[] args) {
+        String[] words = new String[]{"a", "banana", "app", "appl", "ap", "apply", "apple"};
+        longestWordsSolution solution = new longestWordsSolution();
+    }
+}
